90f1a50d621583ba635f76c9353ee430f49e12ac
matheusburey <matheushburei.99@gmail.com>
2022-06-07 11:52:24 -0300

    feat(main): add import firabase config and style global

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import App from "./App";
+
+import "./services/firebase";
+import "./style/global.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
