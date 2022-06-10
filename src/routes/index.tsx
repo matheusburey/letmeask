27e3136d1163235543aa52f3521b3016ef28ed49
matheusburey <matheushburei.99@gmail.com>
2022-06-10 15:46:40 -0300

    feat(routes): created new route to room
    new route to room id

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,14 @@ import { Route, Routes as RoutesReact } from "react-router-dom";
 
 import Home from "../pages/Home";
 import NewRoom from "../pages/NewRoom";
+import Room from "../pages/Room";
 
 function Routes() {
   return (
     <RoutesReact>
       <Route path="/" element={<Home />} />
       <Route path="/rooms/new" element={<NewRoom />} />
+      <Route path="/rooms/:id" element={<Room />} />
     </RoutesReact>
   );
 }
