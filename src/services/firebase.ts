ac5a5884d8a44ba3eb576ec39c12b4cbc2a744c0
matheusburey <matheushburei.99@gmail.com>
2022-06-10 15:47:30 -0300

    feat(services/firebase): create const database

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 
 const {
   VITE_API_KEY,
@@ -25,3 +26,4 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const database = getDatabase(app);
