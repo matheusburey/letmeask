8c5078df4bbc10fe43278c7e45482c91ad71b878
matheusburey <matheushburei.99@gmail.com>
2022-06-11 17:38:14 -0300

    feat(components): button add props
    created props to function

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,13 +6,15 @@ interface IButtonProps {
   children: ReactNode;
   typeButton?: boolean;
   isOutlined?: boolean;
+  callback?: () => void;
 }
 
-function Button({ children, typeButton, isOutlined }: IButtonProps) {
+function Button({ children, typeButton, isOutlined, callback }: IButtonProps) {
   return (
     <button
       className={`button ${isOutlined ? "outlined" : " "}`}
       type={typeButton ? "button" : "submit"}
+      onClick={callback}
     >
       {children}
     </button>
@@ -22,6 +24,7 @@ function Button({ children, typeButton, isOutlined }: IButtonProps) {
 Button.defaultProps = {
   typeButton: false,
   isOutlined: false,
+  callback: undefined,
 };
 
 export default Button;
