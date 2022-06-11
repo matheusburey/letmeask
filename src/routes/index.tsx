752d891093ebd87fb7dbcfef454fec8b6b29a2e1
matheusburey <matheushburei.99@gmail.com>
2022-06-11 17:52:25 -0300

    feat(routes): add routes admin

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes as RoutesReact } from "react-router-dom";
 
+import AdminRoom from "../pages/AdminRoom";
 import Home from "../pages/Home";
 import NewRoom from "../pages/NewRoom";
 import Room from "../pages/Room";
@@ -10,6 +11,7 @@ function Routes() {
       <Route path="/" element={<Home />} />
       <Route path="/rooms/new" element={<NewRoom />} />
       <Route path="/rooms/:id" element={<Room />} />
+      <Route path="/admin/rooms/:id" element={<AdminRoom />} />
     </RoutesReact>
   );
 }
