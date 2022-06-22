4061a12c19ecdf61ad28eb1fd0b67874fd7c2dc0
matheusburey <matheushburei.99@gmail.com>
2022-06-22 14:13:02 -0300

    refactor(pages/AdminRoom): alteration to chakra Ui
    change from sass to ui chakra

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,5 +1,6 @@
-import { onValue, ref, remove, update } from "firebase/database";
-import { useEffect, useState } from "react";
+import { Badge, Box, Flex, HStack, Image, Text } from "@chakra-ui/react";
+import { ref, remove, update } from "firebase/database";
+import { useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
 import answerImg from "../assets/images/answer.svg";
@@ -11,8 +12,6 @@ import Question from "../components/Question";
 import RoomCode from "../components/RoomCode";
 import { RoomUse } from "../providers/Room";
 import { database } from "../services/firebase";
-
-import "../style/room.scss";
 
 function AdminRoom() {
   const { id } = useParams();
@@ -27,11 +26,8 @@ function AdminRoom() {
     getRoom(id);
   }, [id]);
 
-  const handleEndRoom = (questionId = "") => {
-    if (
-      window.confirm("voce realmente deseja encerar esta sala") &&
-      questionId
-    ) {
+  const handleEndRoom = () => {
+    if (window.confirm("voce realmente deseja encerar esta sala")) {
       remove(ref(database, `rooms/${id}`));
       navigate("/");
     }
@@ -63,58 +59,64 @@ function AdminRoom() {
   };
 
   return (
-    <div id="page_room">
-      <header>
-        <div className="content">
-          <img src={logoImg} alt="Letmeask" />
-          <div>
+    <>
+      <Box as="header" p="6" borderBottom="#e2e8f0 solid 1px">
+        <Flex maxW="1120px" mx="auto" align="center" justify="space-between">
+          <Image maxH="45px" src={logoImg} alt="Letmeask" />
+          <HStack spacing="4">
             <RoomCode code={id} />
-            <Button callback={handleEndRoom} isOutlined typeButton>
+            <Button variant="outline" w="auto" h="40px" onClick={handleEndRoom}>
               Encerar sala
             </Button>
-          </div>
-        </div>
-      </header>
-      <main>
-        <div className="room_title">
-          <h1>Sala {title}</h1>
-          <span>{questions?.length} perguntas</span>
-        </div>
-        {questions?.map(({ id, isHighlighted, isAnswered, author, content }) => {
-          return (
-            <Question
-              key={id}
-              author={author}
-              isHighlighted={isHighlighted}
-              isAnswered={isAnswered}
-              content={content}
-            >
-              <button
-                type="button"
-                aria-label="Marcar pergunta pergunta como respondida"
-                onClick={() => handleCheckQuestionAnswer(id)}
+          </HStack>
+        </Flex>
+      </Box>
+      <Box as="main" maxW="800px" mx="auto">
+        <Flex mt="8" mb="6">
+          <Text as="h1" fontWeight="bold" fontSize="2xl" fontFamily="poppins">
+            Sala {title}
+          </Text>
+          <Badge ml="4" py="2" px="3" colorScheme="purple" borderRadius="full">
+            {questions?.length} perguntas
+          </Badge>
+        </Flex>
+        {questions?.map(
+          ({ id, isHighlighted, isAnswered, author, content }) => {
+            return (
+              <Question
+                key={id}
+                author={author}
+                isHighlighted={isHighlighted}
+                isAnswered={isAnswered}
+                content={content}
               >
-                <img src={checkImg} alt="Remover pergunta" />
-              </button>
-              <button
-                type="button"
-                aria-label="Dar pergunta destaque a pergunta"
-                onClick={() => handleHighlightQuestion(id)}
-              >
-                <img src={answerImg} alt="Remover pergunta" />
-              </button>
-              <button
-                type="button"
-                aria-label="Remover pergunta"
-                onClick={() => handleDeleteQuestion(id)}
-              >
-                <img src={deleteImg} alt="Remover pergunta" />
-              </button>
-            </Question>
-          );
-        })}
-      </main>
-    </div>
+                <button
+                  type="button"
+                  aria-label="Marcar pergunta pergunta como respondida"
+                  onClick={() => handleCheckQuestionAnswer(id)}
+                >
+                  <img src={checkImg} alt="Remover pergunta" />
+                </button>
+                <button
+                  type="button"
+                  aria-label="Dar pergunta destaque a pergunta"
+                  onClick={() => handleHighlightQuestion(id)}
+                >
+                  <img src={answerImg} alt="Remover pergunta" />
+                </button>
+                <button
+                  type="button"
+                  aria-label="Remover pergunta"
+                  onClick={() => handleDeleteQuestion(id)}
+                >
+                  <img src={deleteImg} alt="Remover pergunta" />
+                </button>
+              </Question>
+            );
+          }
+        )}
+      </Box>
+    </>
   );
 }
 
