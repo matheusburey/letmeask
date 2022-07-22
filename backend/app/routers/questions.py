38dfc4108b5f33acbe8ceb9d88ed89d7e9d897e2
matheusburey <matheushburei.99@gmail.com>
2022-07-22 10:31:31 -0300

    config backend websockert

--- backend/app/routers/questions.py
+++ backend/app/routers/questions.py
@@ -1,9 +1,8 @@
 from fastapi import APIRouter
+
 from app.controlers import questions
 
 rout_questions = APIRouter(prefix="/questions")
 
 
-rout_questions.post("")(questions.create_new_question)
-rout_questions.get("/{room_id}")(questions.get_all_questions)
-rout_questions.delete("/{room_id}")(questions.delete_questions)
+rout_questions.websocket("/{room_id}")(questions.ws)
