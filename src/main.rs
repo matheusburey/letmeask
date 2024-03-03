19f5cdaa351089692234a1f6a070bfd2ea3ef28c
matheusburey <matheushburei.99@gmail.com>
2024-03-03 13:51:50 -0300

    config base leptos

---
+++ src/main.rs
@@ -0,0 +1,51 @@
+use axum::{
+    extract::ws::{WebSocket, WebSocketUpgrade},
+    response::{IntoResponse, Response},
+    routing::get,
+    Router,
+};
+
+pub async fn handler(ws: WebSocketUpgrade) -> Response {
+    ws.on_upgrade(handle_socket)
+}
+
+async fn handle_socket(mut socket: WebSocket) {
+    while let Some(msg) = socket.recv().await {
+        let msg = if let Ok(msg) = msg {
+            msg
+        } else {
+            // client disconnected
+            return;
+        };
+
+        if socket.send(msg).await.is_err() {
+            // client disconnected
+            return;
+        }
+    }
+}
+
+#[cfg(feature = "ssr")]
+#[tokio::main]
+async fn main() {
+    use cruel_doubt::app::App;
+    use cruel_doubt::fileserv::file_and_error_handler;
+    use leptos::*;
+    use leptos_axum::{generate_route_list, LeptosRoutes};
+    let conf = get_configuration(None).await.unwrap();
+    let leptos_options = conf.leptos_options;
+    let addr = leptos_options.site_addr;
+    let routes = generate_route_list(App);
+
+    let app = Router::new()
+        .route("/ws", get(handler))
+        .leptos_routes(&leptos_options, routes, App)
+        .fallback(file_and_error_handler)
+        .with_state(leptos_options);
+
+    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
+    logging::log!("listening on http://{}", &addr);
+    axum::serve(listener, app.into_make_service())
+        .await
+        .unwrap();
+}
