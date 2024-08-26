d64aed688d7fb210555dfa07879edac29779d31d
matheusburey <matheushburei.99@gmail.com>
2024-08-25 23:08:32 -0300

    feat: update create new room

--- src/pages/room/new.vue
+++ src/pages/room/new.vue
@@ -1,20 +1,43 @@
+<script lang="ts" setup>
+const roomTitle = ref("");
+
+async function createNewRoom() {
+    const res = await $fetch(`/api/room`, {
+        method: "POST",
+        body: { title: roomTitle.value.trim() },
+    });
+    console.log(res);
+    if (res?.slug) {
+        await navigateTo({ path: `/room/${res?.slug}/admin` });
+    }
+}
+</script>
 <template>
     <div class="flex min-h-screen">
         <Aside></Aside>
         <main class="basis-1/2 flex justify-center items-center">
             <div class="max-w-80 w-full flex flex-col">
                 <a href="/" class="mx-auto mb-20">
-                    <img width="170" src="~/assets/img/logo.svg" alt="Cruel doubt" />
+                    <img
+                        width="170"
+                        src="~/assets/img/logo.svg"
+                        alt="Cruel doubt"
+                    />
                 </a>
                 <h2 class="text-center font-poppins font-bold text-2xl mb-4">
                     Criar uma nova sala
                 </h2>
-                <Input />
-                <Button></Button>
+                <Input
+                    placeholder="Digite o código da sala"
+                    v-model="roomTitle"
+                />
+                <Button @click="createNewRoom">Criar sala</Button>
 
                 <p class="text-gray-400 text-sm mt-4">
                     Quer entrar em uma sala existente?
-                    <NuxtLink to="/" class="text-purple-600">clique aqui</NuxtLink>
+                    <NuxtLink to="/" class="text-purple-600"
+                        >clique aqui</NuxtLink
+                    >
                 </p>
             </div>
         </main>
--- src/server/api/room/index.post.ts
+++ src/server/api/room/index.post.ts
@@ -3,7 +3,11 @@ import { RoomSchema } from "~/models/room.schema";
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
   try {
-    return await new RoomSchema(body).save();
+    const slug = Array.from({ length: 4 }, () =>
+      Math.floor(Math.random() * 10),
+    );
+
+    return await new RoomSchema({ title: body.title, slug }).save();
   } catch (error) {
     return error;
   }
